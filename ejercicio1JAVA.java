db6c285d078dc2cbd79fff5216826086d41e8348
jesus-espinosa19 <jesusarroyo1064@gmail.com>
2025-04-10 09:25:00 -0500

    Add files via upload

---
+++ ejercicio1JAVA.java
@@ -0,0 +1,26 @@
+public class ejercicio {
+
+    public static void main(String[] args) {
+
+        int num1 = 6;
+        int num2 = 2;
+        
+        int suma = num1 + num2;
+        int resta = num1 - num2;
+        int multiplicacion = num1 * num2;
+        double division = num1 / num2;
+        int modulo = num1 % num2;
+    
+        System.out.println("numero 1: " + num1);
+        System.out.println("numero 2: " + num2);
+        System.out.println("__________________________________________");
+    
+        System.out.println("suma : " + suma);
+        System.out.println("resta : " + resta);
+        System.out.println("multiplicacion : " + multiplicacion);
+        System.out.println("division : " + division);
+        System.out.println("modulo : " + modulo);
+
+    }
+    
+}
---
+++ ejercicio2JAVA.java
@@ -0,0 +1,25 @@
+public class mayor_menor {
+
+    public static void main(String[]args){
+
+        
+        int num1 = 2;
+        int num2 = 2;
+        
+      if (num1 > num2){
+        System.out.println(num1 + " es mayor que " + num2);
+
+      } else if (num2 > num1){
+
+        System.out.println(num2 + " es mayor que " + num1);
+
+      } else {
+        System.out.println("los dos numeros son iguales");
+
+      }
+    
+
+    }
+
+    
+}
